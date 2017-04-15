e6a8d19beb1a183225320409b7de2aa8c41006a4
Josip Digital <info@digriot.com>
2017-04-15 22:30:05 +0200

    ff
    ff

--- scripts/robot.js
+++ scripts/robot.js
@@ -42,8 +42,8 @@ app.controller('work', function ($scope) {
 app.controller('about', function ($scope) { });
 
 app.controller("PolarAreaCtrl", function ($scope) {
-    $scope.labels = ["HTML", "CSS", "JavaScripts", "NodeJS", "Bootstrap", "Angular JS", "PHP", "JQuery", "Ionic"];
-    $scope.data = [90, 90, 70, 60, 100, 75, 35, 65, 55];
+    $scope.labels = ["HTML", "CSS", "JavaScripts", "NodeJS", "Bootstrap", "Angular JS", "PHP", "JQuery", "Ionic", "Photoshop", "Git", "ChartsJS", "Wordpress", "mySQL", "RESTful"];
+    $scope.data = [90, 90, 70, 60, 100, 75, 35, 65, 55, 75, 85, 50, 90, 80, 75];
     $scope.options = {
         legend: {
             display: true,
