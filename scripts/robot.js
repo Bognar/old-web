2f33ebdbe3b94eaa970868d7cf797e111c74ee0f
Josip Digital <info@digriot.com>
2017-04-15 13:26:11 +0200

    change 1.0
    Changes mail form

--- scripts/robot.js
+++ scripts/robot.js
@@ -83,12 +83,6 @@ app.controller('mainCtrl', function ($scope) {
         pimp.add = "jello";
     };
 });
-//app.controller('skill', ['$scope', '$http', function ($scope, $http) {
- //   $http.get("scripts/skills.json")
- //      .then(function (response) {
-   //        $scope.myskills = response.data.skills;
-  //     });
-//}]);
 
 app.controller('pm', function ($scope) { 
 $scope.min = 0;
@@ -101,35 +95,6 @@ $scope.plus = function () {
     $scope.min = $scope.min + 20;
     
 };
-});
-app.controller("EmailCtrl", function ($scope, $http) {
-
-    var mailgunUrl = "sandboxa105b45278d24dadad12e4367aa24c2a.mailgun.org";
-    var mailgunApiKey = window.btoa("api:key-cc24f8939e6324637827ade9250ee2d9")
-
-    $scope.recipient = "josip.bognar@gmail.com";
-    $scope.sender = "";
-
-    $scope.send = function (sender, subject, message) {
-        $http(
-            {
-                "method": "POST",
-                "url": "https://api.mailgun.net/v3/" + mailgunUrl + "/messages",
-                "headers": {
-					"Allow-Control-Allow-Origin": "*",
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": "Basic " + mailgunApiKey
-                },
-                data: "from=" + sender + "&to=josip.bognar@gmail.com" + "&subject=" + subject + "&text=" + message
-            }
-        ).then(function (success) {
-            console.log("SUCCESS " + JSON.stringify(success));
-            $scope.poruka = "E-mail successfuly sent!."
-        }, function (error) {
-            console.log("ERROR " + JSON.stringify(error));
-            $scope.poruka = "Error sending e-mail "
-        });
-    }
 });
 app.controller('postctrl', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
     $http.get('scripts/posts.json').then(function (response) {
